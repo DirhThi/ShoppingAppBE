c3d71efd343f959bcab06fc998d17e258b834d27
Hi <huydao1008@gmail.com>
2024-07-06 02:46:44 +0700

    update order controller

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -19,6 +19,7 @@ const orderController = {
       const { cartItemId, user } = req.body;
       var total = 0;
       var promise = new Promise(function (resolve, reject) {
+        console.log(cartItemId);
         cartItemId.forEach(async function (currentValue, index) {
           const deletedCartItem = await cartItem.findByIdAndUpdate(
             currentValue,
@@ -192,6 +193,42 @@ const orderController = {
     } catch (error) {
       console.log(error);
     }
+  },
+  paymentCallback: async (req, res, next) => {
+    let result = {};
+    console.log(req.body);
+    try {
+      let dataStr = req.body.data;
+      let reqMac = req.body.mac;
+
+      let mac = CryptoJS.HmacSHA256(dataStr, config.key2).toString();
+      console.log("mac =", mac);
+
+      // kiểm tra callback hợp lệ (đến từ ZaloPay server)
+      if (reqMac !== mac) {
+        // callback không hợp lệ
+        result.return_code = -1;
+        result.return_message = "mac not equal";
+      } else {
+        // thanh toán thành công
+        // merchant cập nhật trạng thái cho đơn hàng ở đây
+        let dataJson = JSON.parse(dataStr, config.key2);
+        console.log(
+          "update order's status = success where app_trans_id =",
+          dataJson["app_trans_id"]
+        );
+
+        result.return_code = 1;
+        result.return_message = "success";
+      }
+    } catch (ex) {
+      console.log("lỗi:::" + ex.message);
+      result.return_code = 0; // ZaloPay server sẽ callback lại (tối đa 3 lần)
+      result.return_message = ex.message;
+    }
+
+    // thông báo kết quả cho ZaloPay server
+    res.json(result);
   },
 };
 
