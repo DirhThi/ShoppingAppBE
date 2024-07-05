4da07b559ddd972af448f8bb97581865b0bb8f1b
Hi <huydao1008@gmail.com>
2024-07-05 18:39:56 +0700

    add zalopay

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,7 +1,18 @@
 import { Order, OrderBuilder } from "../models/orderModel.js";
-import   {CartItem as cartItem}  from "../models/cartItemsModel.js";
+import { CartItem as cartItem } from "../models/cartItemsModel.js";
 import { Product } from "../models/productModel.js";
 import { User } from "../models/userModel.js";
+import moment from "moment";
+import axios from "axios";
+import CryptoJS from "crypto-js";
+
+const config = {
+  app_id: "2553",
+  key1: "PcY4iZIKFCIdgZvA6ueMcMHHUbRLYjPL",
+  key2: "kLtgPl8HHhfvMuDHPwKfgfsY4Ydm9eIz",
+  endpoint: "https://sb-openapi.zalopay.vn/v2/create",
+};
+
 const orderController = {
   addNewOrder: async (req, res, next) => {
     try {
@@ -54,7 +65,9 @@ const orderController = {
           .build();
 
         const savedOrder = await newOrder.save();
-        res.status(200).json(savedOrder);
+        req.total = total;
+        next();
+        // res.status(200).json(savedOrder);
       });
     } catch (error) {
       next(error);
@@ -113,6 +126,71 @@ const orderController = {
     } catch (error) {
       console.error("Error updating Order:", error);
       res.status(500).json({ message: "Internal server error" });
+    }
+  },
+
+  createPayment: async (req, res, next) => {
+    const embed_data = {
+      //sau khi hoàn tất thanh toán sẽ đi vào link này
+      redirecturl: "http://localhost:3000/",
+    };
+
+    const items = [
+      {
+        name: "Hat dieu",
+        quantity: 1,
+        price: 60000,
+      },
+      {
+        name: "Hat sen",
+        quantity: 2,
+        price: 30000,
+      },
+    ];
+    const transID = Math.floor(Math.random() * 1000000);
+
+    const total = items.reduce(
+      (acc, item) => acc + item.price * item.quantity,
+      0
+    );
+
+    const order = {
+      app_id: config.app_id,
+      app_trans_id: `${moment().format("YYMMDD")}_${transID}`, // translation missing: vi.docs.shared.sample_code.comments.app_trans_id
+      app_user: "user123",
+      app_time: Date.now(), // miliseconds
+      item: JSON.stringify(items),
+      embed_data: JSON.stringify(embed_data),
+      amount: req.total,
+      //khi thanh toán xong, zalopay server sẽ POST đến url này để thông báo cho server của mình
+
+      description: `Payment for the order #${transID}`,
+      bank_code: "",
+    };
+
+    // appid|app_trans_id|appuser|amount|apptime|embeddata|item
+    const data =
+      config.app_id +
+      "|" +
+      order.app_trans_id +
+      "|" +
+      order.app_user +
+      "|" +
+      order.amount +
+      "|" +
+      order.app_time +
+      "|" +
+      order.embed_data +
+      "|" +
+      order.item;
+    order.mac = CryptoJS.HmacSHA256(data, config.key1).toString();
+
+    try {
+      const result = await axios.post(config.endpoint, null, { params: order });
+
+      return res.status(200).json(result.data);
+    } catch (error) {
+      console.log(error);
     }
   },
 };
